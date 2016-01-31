c2c03e6f307a5957dd9be18b0f2978e09f1fdb63
Tomaž Tekavec <tomaz@codurance.com>
2016-01-31 13:17:55 +0000

    Basic acc. tests implemented.

--- Walter.AcceptanceTests/WebCrawlerShould.cs
+++ Walter.AcceptanceTests/WebCrawlerShould.cs
@@ -1,9 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Walter.AcceptanceTests
 {
     [TestFixture]
     public class WebCrawlerShould
     {
+
+        [Test]
+        public void fire_page_completed_event_after_crawling_starts()
+        {
+            var crawler = new WebCrawler();
+            var wasCalled = false;
+            crawler.RaisePageCrawlCompletedAsync += (o, e) => wasCalled = true;
+
+            crawler.Crawl(new Uri("https://www.xn--jobbrse-d1a.com/list/jobtitle/"));
+
+            Assert.IsTrue(wasCalled);
+        }
+
+        [Test]
+        public void not_fire_page_completed_event_after_crawling_starts_and_url_does_not_responds()
+        {
+            var crawler = new WebCrawler();
+            var wasCalled = false;
+            crawler.RaisePageCrawlCompletedAsync += (o, e) => wasCalled = true;
+
+            crawler.Crawl(new Uri("http://localhost123:456/"));
+
+            Assert.IsFalse(wasCalled);
+        }
+
     }
 }
---
+++ Walter/WebCrawler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using Abot.Crawler;
+using Abot.Poco;
+
+namespace Walter
+{
+    public class WebCrawler
+    {
+        private readonly PoliteWebCrawler _webCrawler;
+        public event EventHandler RaisePageCrawlCompletedAsync;
+
+        public WebCrawler()
+        {
+            _webCrawler = new PoliteWebCrawler();
+            _webCrawler.PageCrawlCompletedAsync += ProcessPageCrawlCompleted;
+        }
+
+        private void ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            if (e.CrawledPage.HttpWebResponse != null && e.CrawledPage.HttpWebResponse.StatusCode == HttpStatusCode.OK)
+            {
+                RaisePageCrawlCompletedAsync?.Invoke(sender, e);
+                var uri = e.CrawledPage.Uri;
+            }
+        }
+
+        public CrawlResult Crawl(Uri uri)
+        {
+            return _webCrawler.Crawl(uri);
+        }
+
+    }
+}
